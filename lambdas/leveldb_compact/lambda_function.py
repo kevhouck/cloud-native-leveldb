de5b3d2e131217bdc4e2d758ac585c3e5c82c87c
Leon Yang <leon.gh.yang@gmail.com>
2017-12-16 18:05:11 +0000

    Use environ to set lambda region and bucket

--- lambdas/leveldb_compact/lambda_function.py
+++ lambdas/leveldb_compact/lambda_function.py
@@ -1,14 +1,15 @@
 import logging
 import tempfile
+import sys
 import subprocess as sp
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
+    logger.info('got event{}'.format(event))
     tmpdir = tempfile.mkdtemp()
     f = open(tmpdir + '/leveldb_merge.input', 'w')
-    f.write(str(event)) 
+    f.write(str(event))
     f.close()
-    res_json = sp.check_output(['./standalone_merger', 'us-west-2', '739bench', tmpdir])
-    logger.info('got event{}'.format(event))
+    res_json = sp.check_output(['./standalone_merger', os.environ['LEVELDB_REGION'], os.environ['LEVELDB_BUCKET'], tmpdir])
     return {"data": res_json}
 
