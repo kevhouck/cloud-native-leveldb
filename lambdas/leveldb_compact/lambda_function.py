2bc7f964dd419b9c605a8f9d8202b3a961b3ec5f
Leon Yang <leon.gh.yang@gmail.com>
2017-12-19 00:21:29 +0000

    fix lambda: write payload instead of whole event

--- lambdas/leveldb_compact/lambda_function.py
+++ lambdas/leveldb_compact/lambda_function.py
@@ -1,14 +1,14 @@
 import logging
 import tempfile
-import sys
+import os
 import subprocess as sp
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
-    logger.info('got event{}'.format(event))
+    logger.info('got event{}'.format(event['data']))
     tmpdir = tempfile.mkdtemp()
     f = open(tmpdir + '/leveldb_merge.input', 'w')
-    f.write(str(event))
+    f.write(event['data'])
     f.close()
     res_json = sp.check_output(['./standalone_merger', os.environ['LEVELDB_REGION'], os.environ['LEVELDB_BUCKET'], tmpdir])
     return {"data": res_json}
