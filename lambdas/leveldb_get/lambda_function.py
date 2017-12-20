3fce3528f7bc70f18c53498ee4af936fdfa411a1
Leon Yang <leon.gh.yang@gmail.com>
2017-12-20 00:11:55 +0000

    Fix lambda_get

--- lambdas/leveldb_get/lambda_function.py
+++ lambdas/leveldb_get/lambda_function.py
@@ -8,24 +8,23 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    logger.info('got event{}'.format(event['data']))
+    logger.info('got event{}'.format(json.dumps(event)))
     tmpdir = tempfile.mkdtemp()
 
-    fnum = json.loads(event['number'])
-    user_key = json.loads(event['user_key'])
-    local_file = '%06d.ldb' % fnum
+    local_file = '%06d.ldb' % event['number']
     file_path = '%s/%s' % (tmpdir, local_file)
 
     s3 = boto3.resource('s3')
     s3.meta.client.download_file(os.environ['LEVELDB_BUCKET'],
             local_file, file_path)
 
-    proc = sp.run(['./standalone_merger', os.environ['LEVELDB_REGION'], os.           environ['LEVELDB_BUCKET'], tmpdir], encoding='utf-8')
-     if proc.returncode == 0:
-         logger.info('got result {}'.format(proc.stdout))
-     elif proc.returncode > 0:
-         logger.info('record is not found, corrupted, or deleted (rc=%d): stdout=%s,   stderr=%s' % (proc.returncode, proc.stdout, proc.stderr))  
-     else:
-         logger.info('process kill by signal %d: stdout=%s, stderr=%s' % (proc.        returncode, proc.stdout, proc.stderr))
-
-    return {'statue': proc.returncode, 'value': proc.stdout}
+    try:
+        value = sp.check_output(['./table_reader', file_path, event['user_key']])
+        logger.info('got result {}'.format(value))
+        return {'status': 0, 'value': value}
+    except sp.CalledProcessError as e:
+        if e.returncode > 0:
+            logger.info('record is not found, corrupted, or deleted (rc=%d): %s' % (e.returncode, e.output))
+        else:
+            logger.info('process kill by signal %d: %s' % (e.returncode, e.output))
+        return {'status': e.returncode, 'value': e.output}
